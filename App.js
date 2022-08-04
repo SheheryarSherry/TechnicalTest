020f3ce2ce2ff273e6ef8d1804b821552430e5c0
Moeez Ahmed <moeez.ahmed@itb.local>
2022-08-04 00:15:11 -0700

    redux set up and configuration

--- App.js
+++ App.js
@@ -7,10 +7,18 @@
  */
 
 import React from 'react';
+import {Provider} from 'react-redux';
 import Root from './src/navigation';
+import configureStore from './src/redux/store';
+
+const store = configureStore();
 
 const App = () => {
-  return <Root />;
+  return (
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  );
 };
 
 export default App;

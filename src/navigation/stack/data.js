c830abf38476e8cf1d572af7a807aa753a42e45c
Moeez Ahmed <moeez.ahmed@itb.local>
2022-08-03 23:48:33 -0700

    code cleaning and stack navigatin done

---
+++ src/navigation/stack/data.js
@@ -0,0 +1,11 @@
+import {HOME} from '../../constants/navigation';
+import Home from '../../screens/home';
+
+// replace the components with you desired screens or you can add more screens dynamically
+export const authScreens = [
+  {
+    id: 1,
+    name: HOME,
+    component: Home,
+  },
+];
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+import Home from './home/index';
+
+module.exports = {
+  Home,
+};
